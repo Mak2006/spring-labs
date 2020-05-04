a11416451ec32eb9bd0f9be3482861eff41f0803
Mak2006 <invalidemail@gmail.com>
2020-05-04 20:39:01 +0100

    Uploading the boot code

---
+++ boot-postrgre-app/code/src/main/java/com/mak2006/apps/bootpostgre/BootpostgreApplication.java
@@ -0,0 +1,13 @@
+package com.mak2006.apps.bootpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootpostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootpostgreApplication.class, args);
+	}
+
+}
---
+++ boot-postrgre-app/code/src/test/java/com/mak2006/apps/bootpostgre/BootpostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mak2006.apps.bootpostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootpostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
